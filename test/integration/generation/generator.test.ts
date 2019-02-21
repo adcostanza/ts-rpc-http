0c9855301b038995fd3d68083b7c1ff2f02acc8c
Adam Costanza <acostanza@averoinc.com>
2019-02-21 08:00:24 -0700

    generate schemas test

---
+++ test/integration/generation/generator.test.ts
@@ -0,0 +1,18 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+import * as fs from 'fs';
+
+test("generate schemas", async () => {
+    const cmd = "npx ts-node src/generator.ts --model test/integration/generation/models.ts --schemas";
+    const {stdout, stderr} = await exec(cmd);
+    if (stderr != '') {
+        console.log(stderr);
+    }
+
+    const files = fs.readdirSync('test/integration/generation/schema');
+    expect(files).toEqual(
+        ['ServiceDefinition.json',
+            'Todo.json',
+            'createTodoRequest.json']);
+});
+
