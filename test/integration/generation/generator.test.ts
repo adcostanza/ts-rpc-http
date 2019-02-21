2ebd5d0bf8ebff8aba74e57b133019a8098ded4f
Adam Costanza <acostanza@averoinc.com>
2019-02-21 08:18:51 -0700

    make generator testable without commands and update version

--- test/integration/generation/generator.test.ts
+++ test/integration/generation/generator.test.ts
@@ -1,17 +1,11 @@
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
 import * as fs from 'fs';
+import {generate} from "../../../src/generator";
 
 describe("generator", () => {
     test("generate schemas", async () => {
         //schema generation can take a long time
         jest.setTimeout(30000);
-        const cmd = "npx ts-node src/generator.ts --model test/integration/generation/models.ts --schemas";
-        const {stdout, stderr} = await exec(cmd);
-        if (stderr != '') {
-            console.log(stderr);
-        }
-
+        await generate({model: "test/integration/generation/models.ts", schemas: true});
         const files = fs.readdirSync('test/integration/generation/schema');
         expect(files).toEqual(
             ['ServiceDefinition.json',
