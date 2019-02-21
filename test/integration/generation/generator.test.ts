39fa041f0df498a6ad988ad7c029f2976910ae6a
Adam Costanza <acostanza@averoinc.com>
2019-02-21 08:01:16 -0700

    organize into describe

--- test/integration/generation/generator.test.ts
+++ test/integration/generation/generator.test.ts
@@ -2,17 +2,18 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 import * as fs from 'fs';
 
-test("generate schemas", async () => {
-    const cmd = "npx ts-node src/generator.ts --model test/integration/generation/models.ts --schemas";
-    const {stdout, stderr} = await exec(cmd);
-    if (stderr != '') {
-        console.log(stderr);
-    }
+describe("generator", () => {
+    test("generate schemas", async () => {
+        const cmd = "npx ts-node src/generator.ts --model test/integration/generation/models.ts --schemas";
+        const {stdout, stderr} = await exec(cmd);
+        if (stderr != '') {
+            console.log(stderr);
+        }
 
-    const files = fs.readdirSync('test/integration/generation/schema');
-    expect(files).toEqual(
-        ['ServiceDefinition.json',
-            'Todo.json',
-            'createTodoRequest.json']);
+        const files = fs.readdirSync('test/integration/generation/schema');
+        expect(files).toEqual(
+            ['ServiceDefinition.json',
+                'Todo.json',
+                'createTodoRequest.json']);
+    });
 });
-
