bd3e338a891603cf27a6398c7bc8d11251b14c96
Adam Costanza <acostanza@averoinc.com>
2019-02-22 08:46:47 -0700

    message

--- src/cli.ts
+++ src/cli.ts
@@ -1,17 +1,17 @@
 const program = require("commander");
-import {generate} from "./generator";
+import { generate } from "./generator";
 
 program
-    .version("0.0.20")
-    .allowUnknownOption()
-    .option("--model <model>", "Specify model file")
-    .option(
-        "--clients <clients>",
-        "The file for the generated clients in the same directory as the models"
-    )
-    .option("--schemas", "Generate json schemas") //TODO make folder selectable
-    .parse(process.argv);
+  .version("0.0.20")
+  .allowUnknownOption()
+  .option("--model <model>", "Specify model file")
+  .option(
+    "--clients <clients>",
+    "The file for the generated clients in the same directory as the models"
+  )
+  .option("--schemas", "Generate json schemas") //TODO make folder selectable
+  .parse(process.argv);
 
 generate(program)
-    .then(console.log)
-    .catch(e => new Error(e));
+  .then(r => console.log("Code generation complete!"))
+  .catch(e => new Error(e));
