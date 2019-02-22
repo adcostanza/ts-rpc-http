cdaaa30b009c52a6808566920583c3bc3964d08a
Adam Costanza <acostanza@averoinc.com>
2019-02-22 08:43:59 -0700

    run tests

--- test/clients.ts
+++ test/clients.ts
@@ -1,25 +1,25 @@
 import Client from '../src/client';
-import {
-    ServiceDefinition,
-    createTodoRequest,
-    Todo,
+import { 
+  ServiceDefinition,
+  createTodoRequest,
+  Todo,
+  createTodoAsyncRequest,
 } from './models';
 
 export class TodoClient {
-    private client: Client<ServiceDefinition>;
+  private client: Client<ServiceDefinition>;
+  constructor(baseURL: string) {
+    this.client = new Client(baseURL);
+  }
 
-    constructor(baseURL: string) {
-        this.client = new Client(baseURL);
-    }
-
-    public createTodo = async (
-        body: createTodoRequest,
-        token?: string
-    ): Promise<Todo> => this.client.call('createTodo', body, token);
-
-    public createTodoAsync = async (
-        body: createTodoRequest,
-        token?: string
-    ): Promise<Todo> => this.client.call('createTodoAsync', body, token);
-
+  public createTodo = async (
+    body: createTodoRequest,
+    token?: string
+  ): Promise<Todo> => this.client.call('createTodo', body, token);
+  
+  public createTodoAsync = async (
+    body: createTodoAsyncRequest,
+    token?: string
+  ): Promise<Todo> => this.client.call('createTodoAsync', body, token);
+  
 }
